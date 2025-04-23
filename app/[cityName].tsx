748809ee0279ab1d6d094c4314c885835488f98f
FehSouza <fernanda.souza142010@gmail.com>
2025-04-23 07:31:59 -0300

    feat: implement CityDetails component with city data fetching and error handling

--- app/[cityName].tsx
+++ app/[cityName].tsx
@@ -1,12 +1,36 @@
-import { useLocalSearchParams } from 'expo-router'
+import { City } from '@/mock/cities/types'
+import { getCity } from '@/services'
+import { useLocalSearchParams, useRouter } from 'expo-router'
+import { useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
 
 export default function CityDetails() {
+  const router = useRouter()
   const { cityName } = useLocalSearchParams()
+  const [cityData, setCityData] = useState<City>()
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    if (!cityName) return
+
+    const fetchCity = async () => {
+      const city = await getCity(cityName as string)
+      if (!city) setError('Cidade não encontrada ou erro na requisição.')
+      if (city) setCityData(city)
+      setLoading(false)
+    }
+
+    fetchCity()
+  }, [])
+
+  if (!cityName) return null
+  if (loading) return <Text>Loading...</Text>
+  if (!cityData || error) return <Text>{error}</Text>
 
   return (
     <View>
-      <Text>{cityName}</Text>
+      <Text>{cityData.city}</Text>
     </View>
   )
 }
---
+++ services/getCity/index.ts
@@ -0,0 +1,18 @@
+import { City } from '@/mock/cities/types'
+
+export const getCity = async (city: string) => {
+  try {
+    const response = await fetch('https://climapp-api.vercel.app/api')
+    if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`)
+
+    const result = (await response.json()) as City[]
+
+    const cityData = result.find((item) => item.city.toLowerCase() === city.toLowerCase())
+    if (!cityData) throw new Error('City not found...')
+
+    return cityData
+  } catch (error) {
+    console.error('file: services/getCity/index.ts ~ getCity ~ error', error)
+    return null
+  }
+}
