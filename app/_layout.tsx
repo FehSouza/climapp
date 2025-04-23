dc03c018c5ec9804f2cc79d88f0b18dd5a6de593
FehSouza <fernanda.souza142010@gmail.com>
2025-04-23 18:16:34 -0300

    feat: update RootLayout to display Loading component while fonts are loading

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { Loading } from '@/components'
 import { theme } from '@/theme'
 import {
   Montserrat_400Regular,
@@ -12,7 +13,7 @@ import { StatusBar, StyleSheet } from 'react-native'
 export default function RootLayout() {
   const [fontsLoaded] = useFonts({ Montserrat_400Regular, Montserrat_600SemiBold, Montserrat_700Bold })
 
-  if (!fontsLoaded) return null
+  if (!fontsLoaded) return <Loading />
 
   return (
     <LinearGradient colors={[theme.colors.blue500, theme.colors.blue700]} style={StyleSheet.absoluteFill}>
